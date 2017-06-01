b2f2f63dbe14e736f313d3bddc2809faeeb9c07a
M3tiii <m1995g@gmail.com>
2017-06-01 22:28:06 +0200

    Append components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FolderViewComponent } from './folder-view/folder-view.component';
+import { FileViewComponent } from './file-view/file-view.component';
+
+import { LoaderService } from './services/loader.service';
+
+import { ModalModule } from 'ng2-bootstrap';
+import { FoldersPanelComponent } from './folders-panel/folders-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FolderViewComponent,
+    FileViewComponent,
+    FoldersPanelComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [LoaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
