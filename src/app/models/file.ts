535271355973e2e90cdab39d64b969788a01f702
M3tiii <m1995g@gmail.com>
2017-06-01 22:26:03 +0200

    Add Folder, File interface.

---
+++ src/app/models/file.ts
@@ -0,0 +1,6 @@
+export interface File {
+  name: string;
+  format: string;
+  selected?: boolean;
+  edited?: boolean;
+}
---
+++ src/app/models/folder.ts
@@ -0,0 +1,6 @@
+import { File } from './file';
+
+export interface Folder {
+  name: string;
+  files: File[];
+}
