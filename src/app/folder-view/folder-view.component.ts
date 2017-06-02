fc8c1ba7527565466f3523f755a183a6538e75dd
M3tiii <m1995g@gmail.com>
2017-06-02 03:31:41 +0200

    Show modal on delete, fixed method visibility.

--- src/app/folder-view/folder-view.component.ts
+++ src/app/folder-view/folder-view.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { LoaderService } from '../services/loader.service';
-
 import { Folder } from '../models/folder';
 import { File } from '../models/file';
 
@@ -10,18 +9,19 @@ import { File } from '../models/file';
   styleUrls: ['./folder-view.component.css'],
   inputs: ['folder']
 })
-export class FolderViewComponent implements OnInit {
-  folder: Folder;
-  selectAllValue: boolean = false;
-  isAnySelected: boolean = false;
-  isCreatedFile: boolean = false;
+export class FolderViewComponent {
+
+  private folder: Folder;
+  private selectAllValue: boolean = false;
+  private isAnySelected: number = 0;
+  private isCreatedFile: boolean = false;
 
   @Output() modalEmitter = new EventEmitter();
 
   constructor(private service: LoaderService) { }
 
-  modal(a) {
-    this.modalEmitter.emit(a);
+  private showModal(modal) {
+    this.modalEmitter.emit(modal);
   }
 
   private onSelectAll() {
@@ -32,8 +32,9 @@ export class FolderViewComponent implements OnInit {
   }
 
   private refreshSelect(file: File = null) {
-    this.isAnySelected = this.getSelectedFiles().length > 0;
-    this.selectAllValue = this.isAnySelected ? this.selectAllValue : false;
+    this.isAnySelected = this.getSelectedFiles().length;
+    const isAllSelected = this.isAnySelected === this.folder.files.length;
+    this.selectAllValue = this.isAnySelected ? (isAllSelected ? true : this.selectAllValue) : false;
   }
 
   private onRenameSelected() {
@@ -41,13 +42,17 @@ export class FolderViewComponent implements OnInit {
   }
 
   private onDeleteSelected() {
+    this.showModal({ title: "DELETE", msg: "Are you sure you want to delete selected files?", confirm: true, callback: this.confirmedDelete.bind(this) });
+  }
+
+  private confirmedDelete() {
     this.getSelectedFiles().forEach(file => this.deleteFile(file));
     this.refreshSelect();
   }
 
   private onCreateFile() {
     this.isCreatedFile = true;
-    this.folder.files.push(<File>{ edited: true });
+    this.folder.files.unshift(<File>{ edited: true });
   }
 
   private getSelectedFiles(): File[] {
@@ -60,8 +65,4 @@ export class FolderViewComponent implements OnInit {
       this.folder.files.splice(index, 1);
     }
   }
-
-  ngOnInit() {
-  }
-
 }
